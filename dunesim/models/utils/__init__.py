8f50fae41fd954e7ca371556d0608fdcfb30a35b
Irwindeep <b22ai022@iitj.ac.in>
2025-10-13 06:33:33 +0530

    add timesteps device

--- dunesim/models/utils/__init__.py
+++ dunesim/models/utils/__init__.py
@@ -9,7 +9,9 @@ from dunesim.models.utils.conv import UpSample
 def timestep_embd(timesteps: torch.Tensor, embd_dim: int) -> torch.Tensor:
     half_dim = embd_dim // 2
     embd = math.log(10000.0) / (half_dim - 1)
-    embd = torch.exp(-embd * torch.arange(half_dim, dtype=torch.float))
+    embd = torch.exp(
+        -embd * torch.arange(half_dim, dtype=torch.float, device=timesteps.device)
+    )
     embd = timesteps.float().unsqueeze(1) * embd.unsqueeze(0)
 
     embd = torch.cat([torch.sin(embd), torch.cos(embd)], dim=1)
