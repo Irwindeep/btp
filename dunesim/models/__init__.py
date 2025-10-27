a6fe597297719aa9255a9013a45e49cef90a4d3b
Irwindeep <b22ai022@iitj.ac.in>
2025-10-27 14:04:08 +0530

    update __init__

--- dunesim/models/__init__.py
+++ dunesim/models/__init__.py
@@ -1,8 +1,10 @@
 import dunesim.models.utils as utils
+from dunesim.models.atppnet import ATPPNet
 from dunesim.models.unet3d import UNet3D
 from dunesim.models.mcvd import UNet
 
 __all__ = [
+    "ATPPNet",
     "UNet",
     "UNet3D",
     "utils",
