3349d14c83ed1d1326c36cc030b8312192b10a1e
Irwindeep <b22ai022@iitj.ac.in>
2025-10-27 18:01:31 +0530

    Implement Linear attention module for ATPPNet

--- dunesim/models/utils/attn.py
+++ dunesim/models/utils/attn.py
@@ -27,3 +27,47 @@ class AttentionBlock(nn.Module):
 
         output = torch.einsum("abcde,afde->afbc", energy, v)
         return output
+
+
+class LinearAttentionBlock(nn.Module):
+    def __init__(self, in_features: int) -> None:
+        super(LinearAttentionBlock, self).__init__()
+
+        self.W_l = nn.Conv2d(in_features, in_features, kernel_size=1)
+        self.W_g = nn.Conv2d(in_features, in_features, kernel_size=1)
+
+        self.gate_c = nn.Sequential(
+            nn.Conv1d(2 * in_features, in_features, kernel_size=1),
+            nn.InstanceNorm2d(in_features),
+            nn.ReLU(),
+        )
+
+        self.gate_s = nn.Sequential(
+            nn.Conv2d(2 * in_features, in_features // 4, kernel_size=1),
+            nn.InstanceNorm2d(in_features // 4),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_features // 4, in_features // 4, kernel_size=3, padding=4, dilation=4
+            ),
+            nn.InstanceNorm2d(in_features // 4),
+            nn.ReLU(),
+            nn.Conv2d(in_features // 4, 1, kernel_size=1),
+        )
+
+    def forward(self, input: torch.Tensor, g: torch.Tensor) -> torch.Tensor:
+        N, T, C, H, W = input.size()
+        c = torch.zeros((N, C, H, W), device=input.device)
+
+        for t in range(T):
+            layer_output = input[:, t]
+            l_ = self.W_l(layer_output)
+            g_ = self.W_g(g)
+            cat_f = nn.functional.relu(torch.cat([l_, g_], dim=1))
+
+            avg_f = nn.functional.adaptive_avg_pool2d(cat_f, 1).view(N, 2 * C, 1)
+            avg_f = self.gate_c(avg_f).view(N, C, 1, 1)
+
+            s_gate = torch.sigmoid(self.gate_s(cat_f))
+            c = c + layer_output * torch.sigmoid(avg_f * s_gate)
+
+        return c
